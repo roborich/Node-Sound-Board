0e0344ded964aa9a32508a2d8d5565dbebf51f10
Rich Howell <gustove@gmail.com>
2015-04-23 02:19:14 -0400

    changed css

--- public/js/sounds.js
+++ public/js/sounds.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('li').click(function(){
+	$('.sound').click(function(){
 		var sound = $(this).data('file').substring( 0, $(this).data('file').indexOf('.mp3') );
 		$.get( "audio/" + sound, function( data ) {
 
