08951535f26237f8fcd7876a93ac0d6e7b7b689a
Rich Howell <gustove@gmail.com>
2015-04-27 17:52:26 -0400

    rearranged index.js

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ var jade = require('jade');
 var route = require('./routes.js');
 var	bodyParser = require('body-parser');
 
+// Jade engine stuff
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
-app.use(bodyParser.json());
 app.engine('jade', jade.__express);
 
+// Middleware that parses json.
+app.use(bodyParser.json());
+
+//Routes
 app.get('/', route.home);
 app.get('/play/:sound', route.get_play);
 app.post('/audio', route.post_play);
+
+// Middleware that sends static requests.
 app.use(express.static(__dirname + '/public'));
 
+// Initialize Server.
 http.listen(80, function(){
   console.log('listening on *:80');
 });
